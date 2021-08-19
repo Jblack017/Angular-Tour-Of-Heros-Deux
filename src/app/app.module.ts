72797ac2c8186cdafef0e2c48c1069e17d12c934
Jblack017 <blackhamjoshua@comcast.net>
2021-08-19 11:48:32 -0600

    adds hero search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 @NgModule({
   imports: [
     BrowserModule,
@@ -30,6 +31,7 @@ import { AppRoutingModule } from './app-routing.module';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
+    HeroSearchComponent,
   ],
   bootstrap: [AppComponent],
 })
